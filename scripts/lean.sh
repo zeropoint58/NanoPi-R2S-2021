82767122765c31d40839b8ab1df01d7577a169c2
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-04-15 12:26:40 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -18,7 +18,7 @@ sed -i 's,1608,1800,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-def
 sed -i 's,2016,2208,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/cpufreq
 sed -i 's,1512,1608,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/cpufreq
 rm -rf ./target/linux/rockchip/armv8/base-files/etc/hotplug.d
-
+rm -rf ./package/feeds/packages/dockerd
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
