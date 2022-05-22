e62958ce28456b640f3b19b8c4629dcfef1be0c0
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-05-22 10:06:14 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -159,7 +159,3 @@ rm -rf package/libs/wolfssl
 svn co https://github.com/Boos4721/openwrt/trunk/package/libs/wolfssl package/libs/wolfssl
 
 echo -e " DHDAXCW's FusionWrt built on "$(date +%Y.%m.%d)"\n -----------------------------------------------------" >> package/base-files/files/etc/banner
-echo 'net.bridge.bridge-nf-call-iptables=0' >> package/base-files/files/etc/sysctl.conf
-echo 'net.bridge.bridge-nf-call-ip6tables=0' >> package/base-files/files/etc/sysctl.conf
-echo 'net.bridge.bridge-nf-call-arptables=0' >> package/base-files/files/etc/sysctl.conf
-echo 'net.bridge.bridge-nf-filter-vlan-tagged=0' >> package/base-files/files/etc/sysctl.conf
